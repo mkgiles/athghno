9b2879c0954b5ec93975ef068e3a3fe98c3ea523
Conor James Giles <conor@mkgiles.net>
2025-01-12 20:04:51 +0000

    Add Extended Actor types.

---
+++ internal/streams/Actor.go
@@ -0,0 +1,21 @@
+package streams
+
+type PersonAS2 struct {
+	ObjectAS2
+}
+
+type ApplicationAS2 struct {
+	ObjectAS2
+}
+
+type OrganizationAS2 struct {
+	ObjectAS2
+}
+
+type GroupAS2 struct {
+	ObjectAS2
+}
+
+type ServiceAS2 struct {
+	ObjectAS2
+}
