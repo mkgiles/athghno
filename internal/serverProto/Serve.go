039f1ff5354ae4cfc97e434fede0b61b8f7022e9
Conor James Giles <conor@mkgiles.net>
2025-01-23 22:28:36 +0000

    Validate all actor type signatures

--- internal/serverProto/Serve.go
+++ internal/serverProto/Serve.go
@@ -25,24 +25,40 @@ func ValidateSignature(registry *streams.TypeRegistry, db *badger.DB, signature 
 	if err != nil {
 		return nil, err
 	}
+	var pubKey streams.PublicKey
 	switch res.(type) {
 	case streams.PersonAS2:
 		person := res.(streams.PersonAS2)
-		pubKey := person.PublicKey.Complex.(streams.PublicKey)
-		block, _ := pem.Decode([]byte(pubKey.PublicKeyPem))
-		if block == nil {
-			return nil, errors.New("failed to decode public key")
-		}
-		key, err := x509.ParsePKCS1PublicKey(block.Bytes)
-		if err != nil {
-			return nil, err
-		}
-		err = rsa.VerifyPKCS1v15(key, crypto.SHA256, []byte(signature), []byte(partMap["signature"]))
-		if err != nil {
-			return nil, err
-		}
-		return person, nil
+		pubKey = person.PublicKey.Complex.(streams.PublicKey)
+	case streams.ApplicationAS2:
+		app := res.(streams.ApplicationAS2)
+		pubKey = app.PublicKey.Complex.(streams.PublicKey)
+	case streams.GroupAS2:
+		group := res.(streams.GroupAS2)
+		pubKey = group.PublicKey.Complex.(streams.PublicKey)
+	case streams.ServiceAS2:
+		service := res.(streams.ServiceAS2)
+		pubKey = service.PublicKey.Complex.(streams.PublicKey)
+	case streams.OrganizationAS2:
+		org := res.(streams.OrganizationAS2)
+		pubKey = org.PublicKey.Complex.(streams.PublicKey)
+	case streams.ActorAS2:
+		actor := res.(streams.ActorAS2)
+		pubKey = actor.PublicKey.Complex.(streams.PublicKey)
 	default:
 		return nil, errors.New("unknown type")
 	}
+	block, _ := pem.Decode([]byte(pubKey.PublicKeyPem))
+	if block == nil {
+		return nil, errors.New("failed to decode public key")
+	}
+	key, err := x509.ParsePKCS1PublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	err = rsa.VerifyPKCS1v15(key, crypto.SHA256, []byte(signature), []byte(partMap["signature"]))
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
